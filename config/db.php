619c5f7c5ee4db62c2d6f68948e2cb0a677293ad
haivq <aperture147@gmail.com>
2020-11-11 17:08:21 +0700

    Remove duplicated directory

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=pvpgn',
+    'dsn' => 'mysql:host=localhost:3306;dbname=pvpgn',
     'username' => 'root',
-    'password' => '',
+    'password' => 'Lemongrass!',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
