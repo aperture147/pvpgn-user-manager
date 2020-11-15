6519c98ee544a523bad20127237624d7ec147085
Aperture <aperture147@gmail.com>
2020-11-16 01:30:51 +0700

    inval the sengrid asm

--- docker/web/index.php
+++ docker/web/index.php
@@ -7,6 +7,6 @@ $config = require __DIR__ . '/../config/web.php';
 
 defined('SENDGRID_TEMPLATE_ID') or define('SENDGRID_TEMPLATE_ID', getenv('SENDGRID_TEMPLATE_ID'));
 defined('SENDGRID_APIKEY') or define('SENDGRID_APIKEY', getenv('SENDGRID_APIKEY'));
-defined('SENDGRID_ASM') or define('SENDGRID_ASM', getenv('SENDGRID_ASM'));
+defined('SENDGRID_ASM') or define('SENDGRID_ASM', intval(getenv('SENDGRID_ASM')));
 
 (new yii\web\Application($config))->run();
