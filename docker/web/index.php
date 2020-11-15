33bd599db5341e4b8b3ccece5ba431ec4e7d9fbc
Aperture <aperture147@gmail.com>
2020-11-15 21:49:56 +0700

    Remove credentials

--- docker/web/index.php
+++ docker/web/index.php
@@ -5,8 +5,8 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
 
-defined('SENDGRID_TEMPLATE_ID') or define('SENDGRID_TEMPLATE_ID', 'd-bd71054894154f7a940dd0a8fa21e18b');
-defined('SENDGRID_APIKEY') or define('SENDGRID_APIKEY', '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');
-defined('SENDGRID_ASM') or define('SENDGRID_ASM', 15001);
+defined('SENDGRID_TEMPLATE_ID') or define('SENDGRID_TEMPLATE_ID', getenv('SENDGRID_TEMPLATE_ID'));
+defined('SENDGRID_APIKEY') or define('SENDGRID_APIKEY', getenv('SENDGRID_APIKEY'));
+defined('SENDGRID_ASM') or define('SENDGRID_ASM', getenv('SENDGRID_ASM'));
 
 (new yii\web\Application($config))->run();
