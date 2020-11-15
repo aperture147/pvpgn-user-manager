ebd8c033a17d009e21ebe9342a259935d685d66f
Aperture <aperture147@gmail.com>
2020-11-15 21:30:19 +0700

    Delete Vagrantfile

--- config/db.php
+++ config/db.php
@@ -2,13 +2,16 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost:3306;dbname=pvpgn',
-    'username' => 'root',
-    'password' => 'Lemongrass!',
+    'dsn' => 'mysql:host=103.232.123.153:3306;dbname=pvpgn',
+    'username' => 'pvpgn',
+    'password' => 'UFfK5H4nxrCX8qub',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     'enableSchemaCache' => true,
     'schemaCacheDuration' => 60,
     'schemaCache' => 'cache',
+
+    'enableQueryCache' => true,
+    'queryCacheDuration' => 3600,
 ];
