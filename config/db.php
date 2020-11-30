471da2f0d766f09a31f7b679ed6c48392ffced2d
Aperture <aperture147@gmail.com>
2020-11-30 11:25:59 +0700

    remove db

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=103.232.123.153:3306;dbname=pvpgn',
+    'dsn' => 'mysql:host=localhost;dbname=pvpgn',
     'username' => 'pvpgn',
-    'password' => 'UFfK5H4nxrCX8qub',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
