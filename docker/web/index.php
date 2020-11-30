8269e0ccaf0926bac5501eaf3271b44a5e60dce7
Aperture <aperture147@gmail.com>
2020-11-30 12:02:27 +0700

    fix mising defined variable

--- docker/web/index.php
+++ docker/web/index.php
@@ -6,6 +6,7 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 $config = require __DIR__ . '/../config/web.php';
 
 defined('SENDGRID_TEMPLATE_ID') or define('SENDGRID_TEMPLATE_ID', getenv('SENDGRID_TEMPLATE_ID'));
+defined('SENDGRID_VERIFIED_TEMPLATE_ID') or define('SENDGRID_VERIFIED_TEMPLATE_ID', getenv('SENDGRID_TEMPLATE_ID'));
 defined('SENDGRID_APIKEY') or define('SENDGRID_APIKEY', getenv('SENDGRID_APIKEY'));
 defined('SENDGRID_ASM') or define('SENDGRID_ASM', intval(getenv('SENDGRID_ASM')));
 
